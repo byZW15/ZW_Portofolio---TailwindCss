c0ad6f8ce4cc098a91864ae8e8da555920e687f3
byZW15 <by.zulwahyu@gmail.com>
2026-09-08 14:56:28 +0800

    Install Prettier Code, Add back-toTop button, fix mobile hamburger menu when click

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 // Navbar Fixed
 // Js carikan nama elemen tag <header>
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+// Scroll Smooth
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if (window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
-
-
-
 
 // Hamburger Menu
 // Js carikan nama id = #hamburger
@@ -21,8 +24,16 @@ const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
 // hamburger mobile
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    // untuk menghilangkan menu ketika di klik
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  // untuk menghilangkan menu ketika di klik
+  navMenu.classList.toggle("hidden");
+});
+
+// Click diluar hamburger auto close
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
